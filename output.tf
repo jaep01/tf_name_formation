a6d6bd18d46ba567c2c088da8ec102c9fa5192dd
jaep01 <99640540+jaep01@users.noreply.github.com>
2024-06-08 00:44:08 -0700

    updated output value

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
-# output "resource_names" {
-#   value = { for k, v in module.create_resource_name : k => v.resource_name }
-# }
-
 output "resource_names" {
-  # value = module.create_resource_name.resource_name
-  value = module.create_resource_name.*
+  value = { for k, v in module.create_resource_name : k => v.resource_name }
 }
+
+# output "resource_names" {
+#   # value = module.create_resource_name.resource_name
+#   value = module.create_resource_name.*
+# }
