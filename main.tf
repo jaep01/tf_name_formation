78d281d4e4db0897c9b4edba37696cb26625be84
jaep01 <99640540+jaep01@users.noreply.github.com>
2024-06-08 22:58:20 -0700

    added mapping

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 module "create_resource_name" {
   source                      = "github.com/jaep01/tf_name_module"
-  base_name_in                = var.base_name
-  resource_type_in            = var.resource_type
+  for_each = var.mapping
+  base_name_in                = each.key
+  resource_type_in            = each.value
   resource_name_max_length_in = var.resource_name_max_length
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,17 @@
 variable "resource_type" {
   type        = string
-  description = "esource type to create. virtual_machine, key_vault, or storage_account are valid. Any other values will generate an error."
+  description = "Resource type to create. virtual_machine, key_vault, or storage_account are valid. Any other values will generate an error."
 }
 
 variable "base_name" {
   type        = string
   description = "Base name of a resource"
+}
+
+variable "mapping" {
+  type = map(string)
+  description = "Mapping of base_name and resource_type"
+  default = {}
 }
 
 variable "resource_name_max_length" {
