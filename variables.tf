5c1f562bf32f72d058684e5119d42f0c286f5c10
jaep01 <99640540+jaep01@users.noreply.github.com>
2024-06-09 01:28:01 -0700

    cleanup

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "resource_type" {
   type        = string
-  description = "Resource type to create. virtual_machine, key_vault, or storage_account are valid. Any other values will generate an error."
+  description = "Resource type to create. Choose from virtual_machine, key_vault, or storage_account."
 }
 
 variable "base_name" {
@@ -11,5 +11,5 @@ variable "base_name" {
 variable "resource_name_max_length" {
   type        = number
   default     = 15
-  description = "Maximum number of characters allowed in the resource name"
+  description = "Maximum number of characters allowed in a resource name"
 }
