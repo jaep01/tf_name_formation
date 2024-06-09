be7a0d769d75aff7db23886ac3f296543628a0ce
jaep01 <99640540+jaep01@users.noreply.github.com>
2024-06-08 23:05:20 -0700

    added resource_mapping var

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "create_resource_name" {
   source                      = "github.com/jaep01/tf_name_module"
-  for_each = var.mapping
+  for_each = var.resource_mapping
   base_name_in                = each.key
   resource_type_in            = each.value
   resource_name_max_length_in = var.resource_name_max_length
